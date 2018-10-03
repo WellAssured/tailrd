abe17ce60a9b4e35abe8dee28a1ef4f0db3ac249
Jack White <jwhite5672@gmail.com>
2018-10-02 21:36:27 -0400

    Updated Cognito Pool settings to use the Live one.

--- src/aws_config.js
+++ src/aws_config.js
@@ -2,8 +2,8 @@ export default {
   Auth: {
     mandatorySignIn: true,
     region: "us-east-1",
-    userPoolId: "us-east-1_D9UgsBCJf",
-    userPoolWebClientId: "49r7n12gsah78psuam1rmkdodb"
+    userPoolId: "us-east-1_B7s7R0tOg",  // "us-east-1_D9UgsBCJf",
+    userPoolWebClientId: "r6top77jmr68s83mq533r1erf"  // "49r7n12gsah78psuam1rmkdodb"
   },
   aws_appsync_graphqlEndpoint: 'https://wsklwjqjajgf7ej4jzr7j3qhla.appsync-api.us-east-1.amazonaws.com/graphql',
   aws_appsync_region: 'us-east-1',
