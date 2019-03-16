e8316269b311815e0f79e18ace27195e731820b0
Jack White <jwhite5672@gmail.com>
2019-03-16 14:07:45 -0400

    Updated the AWS Config to use the new CF-backed Production Infrastructure.

--- src/aws_config.js
+++ src/aws_config.js
@@ -2,10 +2,10 @@ export default {
   Auth: {
     mandatorySignIn: true,
     region: "us-east-1",
-    userPoolId: "us-east-1_B7s7R0tOg",  // "us-east-1_D9UgsBCJf",
-    userPoolWebClientId: "2qaf9b3i1mt2hmpas26os8vabr"  // "49r7n12gsah78psuam1rmkdodb"
+    userPoolId: "us-east-1_B7s7R0tOg",
+    userPoolWebClientId: "23d44ianaamorjiuc6a3bhhcj4"
   },
-  aws_appsync_graphqlEndpoint: 'https://wsklwjqjajgf7ej4jzr7j3qhla.appsync-api.us-east-1.amazonaws.com/graphql',
+  aws_appsync_graphqlEndpoint: 'https://l5rw6pc7jrfxhpkp5kf3vrehqy.appsync-api.us-east-1.amazonaws.com/graphql',
   aws_appsync_region: 'us-east-1',
   aws_appsync_authenticationType: 'AMAZON_COGNITO_USER_POOLS',
 }
